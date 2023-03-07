0834835ac35865ab800069df747b8410bf34b1c7
presidio <kmoorthy@presidio.com>
2023-03-07 16:48:19 +0530

    db config

--- config/database.js
+++ config/database.js
@@ -33,7 +33,7 @@ module.exports = ({ env }) => {
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
+      database: env("DATABASE_NAME", "strapi-database"),
       user: env("DATABASE_USERNAME", ""),
       password: env("DATABASE_PASSWORD", ""),
     },
